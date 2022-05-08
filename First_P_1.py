9941c57f31987a8776b597d185047a935dd9bf9c
ShaposhnikovDV <99795444+DamianSODW@users.noreply.github.com>
2022-05-08 10:33:51 +0300

    Update First_P_1.py

--- First_P_1.py
+++ First_P_1.py
@@ -1,4 +1,11 @@
-name = "Hello world"
+import sys
+import os
 
+# System data
+print("Byte order is: " + sys.byteorder)
+print("Python size is: " + str(sys.getsizeof(1)))
+print(str(sys.version_info))
+print("System platform is: " + sys.platform)
 
-print(name)
+# OS data
+print(os.getcwd()) #cerrent working directory
