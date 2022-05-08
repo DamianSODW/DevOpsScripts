8c73851698b4da56669139fdcc31f5cfad0ac158
ShaposhnikovDV <99795444+DamianSODW@users.noreply.github.com>
2022-05-08 11:20:50 +0300

    Create hi_script.py

---
+++ hi_script.py
@@ -0,0 +1,9 @@
+def say_it():
+    greeting = 'Hello'
+    target = 'Joe'
+    message = greeting + " " + target
+    print(message)
+
+# It`s command line call?
+if __name__ == '__main__':
+    say_it()
